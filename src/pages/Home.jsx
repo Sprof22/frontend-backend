74ff2ae09f88e5d003c300ffffef4034b2fec72d
Sprof22 <richmondelaigwu@gmail.com>
2022-12-02 11:03:29 +0100

    Committed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='flex gap-3 bg-slate-300 p-2 align-center justify-center'>
-      <Link to="/signup" className='bg-slate-500 rounded p-1 hover:bg-sky-700'>SignUP</Link>
-      <Link to="/login" className='bg-slate-500 rounded p-1 hover:bg-sky-700'>LogIn</Link>
+    <div className="signup-container flex flex-col align-center justify-center w-full">
+      <h1 className="text-yellow-400" style={{fontSize: "40px"}}>Welcome to FrontEnd-BackEnd</h1>
+      <h1 className="text-orange-400" style={{fontSize: "40px"}}>What you wanna do?</h1>
+      <div className="links flex gap-3 p-2 align-center justify-center">
+        <Link
+          to="/signup"
+          className="bg-slate-500 rounded p-1 hover:bg-sky-700"
+          >
+          SignUP
+        </Link>
+        <Link to="/login" className="bg-slate-500 rounded p-1 hover:bg-sky-700">
+          LogIn
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
